3f13133293584ca01e5bcbb42d9e2df198721dea
latticejeff <j98728@gmail.com>
2015-09-10 19:08:50 +0800

    upload bankmap project
    upload bankmap project files

---
+++ js/handle.js
@@ -0,0 +1,92 @@
+﻿//District selection event handler.
+var activeDistrictElem = null;
+
+$(".nav.navbar-nav").click(function(e){ 
+	
+	//remove previous active district element.
+	if(activeDistrictElem !== null)
+		$(activeDistrictElem).removeClass("active");
+	
+	//activate current clicked element and store the old element.	
+	$(e.target).parent().addClass("active");
+	activeDistrictElem = $(e.target).parent();
+	
+	//call list function.
+	list($(e.target).parent().data('district'));
+	
+	//for test.
+	console.log($(e.target).html() + 'is clicked.');
+	
+	});
+	
+//list all bank information in given district.	
+function list(district){
+	
+	//clear the previous results.
+	$(".nav.nav-sidebar").empty();
+	
+	//show guide or bank information.
+	if(district === 'Guide')
+		$(".nav.nav-sidebar").append(
+			$("<li><a href='#'>"+ bankInfo[district][0].desc +"</a></li>")
+		  );
+	else{	
+	//fetch bank info.
+	for(var i = 0; i < bankInfo[district].length; i++)
+		$(".nav.nav-sidebar").append(
+		$("<li data-index="+ bankInfo[district][i].index +"><a href='#'>"+ bankInfo[district][i].name +"<br >"+
+								bankInfo[district][i].address+"<br >"+
+								"tel : "+ bankInfo[district][i].tel+"<br >"+
+								"fax : "+ bankInfo[district][i].fax+"<br >"+
+		"</a></li>")
+		);
+	
+	addPanTo(district);	
+	
+	}
+	
+}	
+
+function addPanTo(district){
+
+	$(".nav.nav-sidebar li").hover(
+	
+	function(e){
+	
+		$(e.target).parent().addClass('active');
+		
+		//remove all markers on the map.
+		for (var i = 0; i < markers.length; i++)
+			markers[i].setMap(null);
+		
+		//fetch bank location(lat, lng).
+		var lat = bankInfo[district][parseInt($(e.target).parent().data('index'))].lat;
+		var lng = bankInfo[district][parseInt($(e.target).parent().data('index'))].lng;
+		
+		
+		//create marker for each selected bank.
+		var marker = new google.maps.Marker({
+			position: {lat: lat, lng: lng},
+			map: map,
+			animation: google.maps.Animation.DROP,
+			title: bankInfo[district][parseInt($(e.target).parent().data('index'))].name
+		});
+		
+		//show currently selected marker.
+		marker.setMap(map);
+		
+		//add new marker into array(markers).
+		markers.push(marker);
+			
+		//pan to.
+		map.panTo({lat: lat, lng: lng});
+		map.setZoom(16);
+	},
+	function(e){
+	
+		$(e.target).parent().removeClass('active');
+	
+	});
+	
+}
+	
---
+++ js/map.js
@@ -0,0 +1,30 @@
+﻿var map;
+var markers = [];
+
+!function initMap() {
+
+	  //default map location.
+	  var myLatlng = {lat: 25.0854061, lng: 121.5615012};
+
+	  map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 12,
+		center: myLatlng //地圖預設中心點
+	  });  
+
+	  /*
+	  map.addListener('center_changed', function() {
+		// 3 seconds after the center of the map has changed, pan back to the
+		// marker.3秒後移回marker中心
+		setTimeout(function() {
+		  map.panTo(marker.getPosition());
+		}, 3000);
+	  });
+		*/
+	/*	
+	  marker.addListener('click', function() {
+		map.setZoom(8);
+		map.setCenter(marker.getPosition());
+	  });
+	*/
+	
+	}();
